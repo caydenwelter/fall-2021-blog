16ad5462f983715189040e67aff488d1e7aa812a
caydenwelter <61837138+caydenwelter@users.noreply.github.com>
2021-10-25 19:07:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-var isLoggedIn;
-var name;
-var createPageText = document.getElementById("username").textContent;
-
-function login(){
-    if(!isLoggedIn){
-        name = prompt("Please enter your name:");
-        isLoggedIn = true;
-    }else{
-        alert("You are already logged in!")
-    }
+function setPos(){
+    alert("test");
 }
